c51f2262e56c1ea2b7e1a4c107e625b659593fb5
Mishkat Mustafid <mishkatmustafid@gmail.com>
2023-01-22 07:09:40 +0600

    basic differences between js and ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
-let age: number = 20
-if (age < 50) age += 10
-console.log(age)
+let user: [number, string] = [1, "Mosh"]
+user.push(1) // This is wrong
+console.log(user)
+
+const enum Gender {
+  Male = "m",
+  Female = "f",
+}
+let myGender: Gender = Gender.Male
+console.log(myGender)
+
+function calculateSum(x: number, y = 0): number {
+  let sum = x + y
+  if (sum > 15) return sum
+  return sum * 1.1
+}
+
+let employee: {
+  readonly id: number
+  name: string
+  retire: (date: Date) => void
+} = {
+  id: 1,
+  name: "Mishkat",
+  retire: (date: Date) => {
+    console.log(date)
+  },
+}
