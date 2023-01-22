3ebbbe45ee7f8777adc6c647cec37b827b2592e4
Mishkat Mustafid <mishkatmustafid@gmail.com>
2023-01-22 07:10:11 +0600

    dist updated

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,20 @@
 "use strict";
-let age = 20;
-if (age < 50)
-    age += 10;
-console.log(age);
+let user = [1, "Mosh"];
+user.push(1);
+console.log(user);
+let myGender = "m";
+console.log(myGender);
+function calculateSum(x, y = 0) {
+    let sum = x + y;
+    if (sum > 15)
+        return sum;
+    return sum * 1.1;
+}
+let employee = {
+    id: 1,
+    name: "Mishkat",
+    retire: (date) => {
+        console.log(date);
+    },
+};
 //# sourceMappingURL=index.js.map
